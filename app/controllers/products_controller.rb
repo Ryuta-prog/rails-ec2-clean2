156178253acbe51fa3c7d3456cba42655293ca19
Ryuta-prog <longtaishangcun@gmail.com>
2024-10-06 12:15:23 +0900

    Modified related_products4

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,19 +4,19 @@ class ProductsController < ApplicationController
   def index
     # 全てのプロダクトを取得
     @products = Product.all
-  
+
     # ページネーションを適用して、8つの新しいプロダクトをページごとに表示
     @products = Product.limit(8)
-  
+
     # 商品がなければ、フラッシュメッセージを表示
     flash.now[:notice] = t('products.no_products') if @products.empty?
   end
-  
+
   def show
     # 選択された商品の詳細を表示
     @product = Product.find(params[:id])
 
     # 関連商品を表示
-    @related_products = Product.where(id: [131, 132, 133, 134]).limit(4)
+    @related_products = Product.where(id: [131, 132, 133, 134]).order(:id)
   end
 end
