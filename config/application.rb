a0ededb02f3cc0740c2b8cc7090947f4e030de22
Ryuta-prog <longtaishangcun@gmail.com>
2025-01-18 12:19:09 +0900

    Rubyのバージョンを3.1.4に更新

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module Myapp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+
+    # config.autoload_lib(ignore: %w[assets tasks])
 
     # 国際化の設定
     config.i18n.default_locale = :ja
