5763300ddbe46b03ccd409ba232cb0d8e0392640
Ryuta-prog <longtaishangcun@gmail.com>
2025-01-18 16:57:43 +0900

    Fix autoloader configuration

--- config/application.rb
+++ config/application.rb
@@ -13,13 +13,17 @@ require 'action_view/view_paths'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.autoloader = :classic
-
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Zeitwerkをクラシックモードに変更
+    config.autoloader = :classic
+
+    
+    
+    
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
