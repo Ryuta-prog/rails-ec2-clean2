110bdee5f16b5dffefee87b3927a98f3b4978277
Ryuta-prog <longtaishangcun@gmail.com>
2025-01-20 21:26:59 +0900

    Railsを7.0.8にダウングレードしてActiveRecordの問題を修正

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.0
 
     # Zeitwerkモード（デフォルト）
     config.autoloader = :zeitwerk
