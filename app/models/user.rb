ee7504a0e42a3623a667714fbfa1bea4b98784e2
Ryuta-prog <longtaishangcun@gmail.com>
2025-01-21 13:36:50 +0900

    Add self.attributes_for_inspect = :all to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord
+class User < ApplicationRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         self.attributes_for_inspect = :all
 end
