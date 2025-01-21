c2f5fee7ce0ec5e091914a78cdee25f104868032
Ryuta-prog <longtaishangcun@gmail.com>
2025-01-21 14:31:32 +0900

    config.active_record.attributes_for_inspect = [:id]をコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         self.attributes_for_inspect = :all
 end
