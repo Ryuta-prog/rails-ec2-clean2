b62a535262f3333f8a5ee56cb16e86a447ca040a
Ryuta-prog <longtaishangcun@gmail.com>
2025-01-28 23:09:27 +0900

    (こちらのレビューをお願いします)レビュー後修正_2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,7 +4,7 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
 
-  def add_product(product, quantity = 1)
+  def add_product!(product, quantity = 1)
     current_item = cart_items.find_by(product_id: product.id)
 
     if current_item
