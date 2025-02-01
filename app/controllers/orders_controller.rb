57b4e5be8a6581ab14d3f3dbfb08845cc1d3f0e8
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-01 22:58:01 +0900

    Modifiy orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,33 +1,35 @@
-def create
-  @order = Order.new(order_params)
-  @order.user = current_user
-  @order.total = current_cart.total_price
+class OrdersController < ApplicationController
+  def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.total = current_cart.total_price
 
-  if @order.save
-    # カート内の商品を購入明細として保存
-    current_cart.cart_items.each do |item|
-      OrderItem.create!(
-        order: @order,
-        product_name: item.product.name,
-        price: item.product.price,
-        quantity: item.quantity
-      )
-    end
+    if @order.save
+      # カート内の商品を購入明細として保存
+      current_cart.cart_items.each do |item|
+        OrderItem.create!(
+          order: @order,
+          product_name: item.product.name,
+          price: item.product.price,
+          quantity: item.quantity
+        )
+      end
 
-    # メール送信とエラー処理
+      # メール送信とエラー処理
       OrderMailer.order_confirmation(@order).deliver_now
     rescue => e
       Rails.logger.error "Failed to send email: #{e.message}"
     end
 
-    # カートを削除
-    current_cart.destroy
-    session[:cart_id] = nil
+      # カートを削除
+      current_cart.destroy
+      session[:cart_id] = nil
 
-    flash[:notice] = "購入ありがとうございます"
-    redirect_to root_path
-  else
-    flash[:alert] = "購入処理に失敗しました"
-    render 'carts/show'
+      flash[:notice] = "購入ありがとうございます"
+      redirect_to root_path
+    else
+      flash[:alert] = "購入処理に失敗しました"
+      render 'carts/show'
+    end
   end
 end
