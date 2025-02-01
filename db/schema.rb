d7151065444c05ec82dbaee36887dbe10e35c500
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-01 22:39:09 +0900

    1つ前のコミットでbundle installをし忘れていたので追加

--- db/schema.rb
+++ db/schema.rb
@@ -111,4 +111,5 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_29_072839) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "orders", "users"
 end
