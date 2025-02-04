b11fa4db541984b229f2c6555fe1c3ffb3a76fbb
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-05 00:11:46 +0900

    Restore application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,12 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener('turbo:load', function() {
+    const form = document.querySelector('form');
+    if (form) {
+      form.addEventListener('submit', function(e) {
+        const submitButton = this.querySelector('input[type="submit"]');
+        submitButton.disabled = true;
+      });
+    }
+});
