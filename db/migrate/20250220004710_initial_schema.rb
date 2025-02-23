bd5e8c9ca54a5062ab6c4f9af82175a522d0a3f6
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-23 19:47:46 +0900

    Fix orders table migration

---
+++ db/migrate/20250220004710_initial_schema.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class InitialSchema < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.references :user, null: false, foreign_key: true
+      t.decimal :total, precision: 10, scale: 2
+      t.string :billing_address
+      t.string :state
+      t.string :zip
+      t.timestamps
+    end
+  end
+end
