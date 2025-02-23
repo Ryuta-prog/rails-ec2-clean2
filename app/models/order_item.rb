f52be1a66475f3f464828f0a12f92047eda106ad
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-24 01:15:52 +0900

    Fix model filename typo

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,7 @@
 
 class OrderItem < ApplicationRecord
   belongs_to :order
+  validates :product_name, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
 end
--- app/models/order_itme.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class OrderItem < ApplicationRecord
-  belongs_to :order
-  validates :product_name, presence: true
-  validates :price, presence: true
-  validates :quantity, presence: true
-end
