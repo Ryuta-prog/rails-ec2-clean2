1f9298c9dffa96727dfc5d4facea0352db67dc68
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-24 00:51:56 +0900

    Reset migrations with single schema file

--- db/migrate/20250223155035_create_initial_schema.rb
+++ db/migrate/20250223155035_create_initial_schema.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
-class SetupOrdersTable < ActiveRecord::Migration[7.0]
+class CreateInitialSchema < ActiveRecord::Migration[7.0]
   def change
+    create_table :users do |t|
+      t.string :email, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.timestamps null: false
+    end
+
+    add_index :users, :email, unique: true
+
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
       t.decimal :total, precision: 10, scale: 2
