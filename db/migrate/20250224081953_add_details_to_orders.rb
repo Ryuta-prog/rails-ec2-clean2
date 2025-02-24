34b5d79cd61200b8af8896572032219758a7830f
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-24 18:18:01 +0900

    Refactor orders migration to use bulk changes

--- db/migrate/20250224081953_add_details_to_orders.rb
+++ db/migrate/20250224081953_add_details_to_orders.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 class AddDetailsToOrders < ActiveRecord::Migration[7.0]
   def change
-    add_column :orders, :last_name, :string
-    add_column :orders, :first_name, :string
-    add_column :orders, :email, :string
-    add_column :orders, :billing_address, :string
-    add_column :orders, :address2, :string
-    add_column :orders, :state, :string
-    add_column :orders, :zip, :string
-    add_column :orders, :card_name, :string
+    change_table :orders, bulk: true do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :email
+      t.string :address2
+      t.string :state
+      t.string :zip
+      t.string :card_name
+    end
   end
 end
