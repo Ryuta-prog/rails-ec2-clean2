f48f472c6bb9173d5f23e667e3552d24f67c9935
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-24 17:01:30 +0900

    Add original_price to products

---
+++ db/migrate/20250224080023_add_original_price_to_products.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddOriginalPriceToProducts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :products, :original_price, :decimal, precision: 10, scale: 2
+  end
+end
