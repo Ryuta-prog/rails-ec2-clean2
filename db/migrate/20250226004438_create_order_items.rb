f04383ac957e4d3f947dd5608ab166c96d005178
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-26 09:47:17 +0900

    Add migration to create order_items table

---
+++ db/migrate/20250226004438_create_order_items.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateOrderItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :order_items do |t|
+      t.references :order, foreign_key: true
+      t.string :product_name
+      t.decimal :price
+      t.integer :quantity
+      t.timestamps
+    end
+  end
+end
