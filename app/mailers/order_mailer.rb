6dd8404afdd89857467374253cb5b6ab891e2ef7
Ryuta-prog <longtaishangcun@gmail.com>
2025-02-27 15:58:02 +0900

    Fix OrderMailer confirmation_email method

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class OrderMailer < ApplicationMailer
-  default from: 'longtaishangcun@gmail.com'
-
-  def confirmation_email
-    @order = params[:order]
+  def confirmation_email(order)
+    @order = order
     mail(
       to: @order.email,
       subject: '【おにぎりや】ご注文ありがとうございます'
