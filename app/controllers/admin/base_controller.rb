df83fa8886bd1e5e981de9fdf95ed7712a783b40
Ryuta-prog <longtaishangcun@gmail.com>
2025-03-11 19:11:56 +0900

    Refacor admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,15 +3,13 @@
 module Admin
   class BaseController < ApplicationController
     before_action :basic_auth
-    
+
     private
-    
+
     def basic_auth
-      Rails.logger.info "Admin::BaseController#basic_auth called"
       authenticate_or_request_with_http_basic do |username, password|
-        result = username == 'admin' && password == 'pw'
-        Rails.logger.info "Authentication result: #{result}, Username: #{username}"
-        result
+        username == ENV.fetch('ADMIN_USERNAME', 'admin') &&
+          password == ENV.fetch('ADMIN_PASSWORD', 'pw')
       end
     end
   end
