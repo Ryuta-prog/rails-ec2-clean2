737353903b5fe0850c8a099c9e65a551d95fce66
Ryuta-prog <longtaishangcun@gmail.com>
2025-03-11 18:58:33 +0900

    Use http_basic_authenticate_with for admin authentication

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,13 +3,15 @@
 module Admin
   class BaseController < ApplicationController
     before_action :basic_auth
-
+    
     private
-
+    
     def basic_auth
+      Rails.logger.info "Admin::BaseController#basic_auth called"
       authenticate_or_request_with_http_basic do |username, password|
-        username == ENV.fetch('ADMIN_USERNAME', 'admin') &&
-          password == ENV.fetch('ADMIN_PASSWORD', 'pw')
+        result = username == 'admin' && password == 'pw'
+        Rails.logger.info "Authentication result: #{result}, Username: #{username}"
+        result
       end
     end
   end
