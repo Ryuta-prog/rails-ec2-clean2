0700167c8e1de7a0a1e126e8333b17b6d290959e
Ryuta-prog <longtaishangcun@gmail.com>
2025-04-16 10:19:15 +0900

    fix: メール送信設定の修正

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class OrderMailer < ApplicationMailer
+  default from: 'onigiriya@xs279744.xsrv.jp'
+
   def confirmation_email(order)
     @order = order
-    mail(
-      to: @order.email,
-      subject: t('mailers.order_mailer.confirmation_email.subject')
-    )
+    @user = order.user
+
+    begin
+      mail(
+        to: @order.email,
+        subject: t('mailers.order_mailer.confirmation_email.subject')
+      )
+    rescue StandardError => e
+      Rails.logger.error("メール送信エラー: #{e.message}")
+    end
   end
 end
