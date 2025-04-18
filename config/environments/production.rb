4eb456b0c2cfb45d629c0ab0795aa43f8dceed3e
Ryuta-prog <longtaishangcun@gmail.com>
2025-04-18 20:54:39 +0900

    fix: Loggerの設定を修正しActiveSupport::LoggerSilenceを適用（NoMethodError: undefined method  解消

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,52 +6,24 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.logger = ActiveSupport::Logger.new($stdout)
 
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  # Eager load code on boot.
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
-  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
   config.active_storage.variant_processor = :mini_magick
 
-  # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
   config.session_store :active_record_store,
                        key: '_myapp_session',
@@ -59,28 +31,14 @@ Rails.application.configure do
                        same_site: :lax,
                        expire_after: 1.week
 
-  # Include generic and useful information about system operation, but avoid logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
-
-  # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "myapp_production"
-
   config.action_mailer.perform_caching = false
-
   config.action_mailer.default_options = {
     from: ENV.fetch('DEFAULT_FROM_EMAIL', 'longtaishangcun@gmail.com')
   }
-
   config.action_mailer.default_url_options = { host: ENV.fetch('APP_HOST', 'rails-ec2-0c3ad7f31a09.herokuapp.com') }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV.fetch('SMTP_SERVER', nil),
@@ -92,24 +50,11 @@ Rails.application.configure do
   }
   config.action_mailer.raise_delivery_errors = true
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
-  # Don't log any deprecations.
   config.active_support.report_deprecations = false
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = Logger::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
-
+  # ログ出力を標準出力に設定（Heroku用）
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
@@ -118,11 +63,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # ログ出力を標準出力に設定
-  config.logger = Logger.new($stdout)
-  config.log_level = :info
-
-  # 静的アセットサーバーを有効化
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 end
