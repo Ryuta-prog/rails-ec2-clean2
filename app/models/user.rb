5d0ef4431b612cbf0d1dc1a9fc37b620f77e3d73
Ryuta-prog <longtaishangcun@gmail.com>
2025-04-22 09:20:24 +0900

    Modify:app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@
 
 class User < ApplicationRecord
   has_many :orders, dependent: :destroy
+  has_many :promotion_codes, dependent: :destroy
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :last_name, presence: true
   validates :first_name, presence: true
 end
