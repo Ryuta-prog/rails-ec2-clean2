742f8d393752cd30dabf466d94d01c398d511db0
Ryuta-prog <longtaishangcun@gmail.com>
2025-05-23 01:12:46 +0900

    fix:app/mailers/order_mailer.rbを修正

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,6 +3,6 @@
 class OrderMailer < ApplicationMailer
   def confirmation_email(order)
     @order = order
-    mail(to: @order.email, subject: I18n.t('order_mailer.confirmation_email.subject'))
+    mail(to: @order.email, subject: I18n.t('mailers.order_mailer.confirmation_email.subject'))
   end
 end
