b1424d21d9ee35e0169274ce4ecea29f64f54e0c
Ryuta-prog <longtaishangcun@gmail.com>
2025-05-22 14:12:20 +0900

    fix:確認メールが届くように修正、不要なマイグレーションファイルを削除

--- db/migrate/20250522015211_make_promotion_codes_user_optional.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class MakePromotionCodesUserOptional < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :promotion_codes, :user_id, true
-  end
-end
