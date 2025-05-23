728b27ae6e0678d843e0ef02516398889146d261
Ryuta-prog <longtaishangcun@gmail.com>
2025-05-23 09:36:32 +0900

    modify:app/mailers/order_mailer.rbを修正

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class OrderMailer < ApplicationMailer
-  def confirmation_email(order)
-    @order = order
+  def confirmation_email
+    @order = params[:order]
     mail(to: @order.email, subject: I18n.t('mailers.order_mailer.confirmation_email.subject'))
   end
 end
