79ab562338bd740e90f822fe8958d874b686cbec
Ryuta-prog <longtaishangcun@gmail.com>
2025-06-04 01:02:48 +0900

    Fix: cart_items_controller.rbとorders_controller.rb修正

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -10,20 +10,29 @@ class CartItemsController < ApplicationController
     @cart_item.quantity = @cart_item.new_record? ? quantity : @cart_item.quantity + quantity
 
     if @cart_item.save
-      redirect_to(request.referer || products_path, notice: t('.success', product_name: @product.name))
+      redirect_to(request.referer || products_path,
+                  notice: t('controllers.cart_items.create.success',
+                            product_name: @product.name))
     else
-      redirect_to products_path, alert: t('.failure')
+      redirect_to products_path,
+                  alert: t('controllers.cart_items.create.failure')
     end
   rescue ActiveRecord::RecordNotFound
-    redirect_to products_path, alert: t('.product_not_found')
+    redirect_to products_path,
+                alert: t('controllers.cart_items.create.product_not_found')
   end
 
   def destroy
     if @cart_item.destroy
-      redirect_to cart_path, notice: t('.item_removed')
+      redirect_to cart_path,
+                  notice: t('controllers.cart_items.destroy.item_removed')
     else
-      redirect_to cart_path, alert: t('.item_remove_failed')
+      redirect_to cart_path,
+                  alert: t('controllers.cart_items.destroy.item_remove_failed')
     end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to cart_path,
+                alert: t('controllers.cart_items.destroy.item_not_found')
   end
 
   private
@@ -31,12 +40,14 @@ class CartItemsController < ApplicationController
   def find_product
     @product = Product.find(params[:cart_item][:product_id])
   rescue ActiveRecord::RecordNotFound
-    redirect_to products_path, alert: t('.product_not_found')
+    redirect_to products_path,
+                alert: t('controllers.cart_items.create.product_not_found')
   end
 
   def find_cart_item
     @cart_item = @cart.cart_items.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    redirect_to cart_path, alert: t('.item_not_found')
+    redirect_to cart_path,
+                alert: t('controllers.cart_items.destroy.item_not_found')
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,38 +5,34 @@ class OrdersController < ApplicationController
 
   # チェックアウト実行
   def create
-    # モデル側のクラスメソッドで一括注文作成＋後処理を実行
     @order = Order.create_with_cart!(
       cart: @cart,
       promo_id: session[:applied_promotion_code_id],
       order_params: order_params
     )
 
-    # 注文成功時は商品一覧へリダイレクトし、クーポンコードをフラッシュに表示
-    redirect_to products_path, notice: t('.success_with_coupon', coupon: @order.next_coupon_code)
+    redirect_to products_path,
+                notice: t('controllers.orders.create.success_with_coupon',
+                          coupon: @order.next_coupon_code)
 
-    # セッションからカートIDとプロモーションコードIDを削除
     session.delete(:cart_id)
     session.delete(:applied_promotion_code_id)
   rescue ActiveRecord::RecordInvalid
-    # バリデーションエラーの場合はカート画面を再表示（422）
-    flash.now[:alert] = t('.failure')
+    flash.now[:alert] = t('controllers.orders.create.failure')
     render 'carts/show', status: :unprocessable_entity
   rescue StandardError => e
-    # 想定外エラー時はログ出力し、カート画面を再表示（500）
-    Rails.logger.error(I18n.t('orders.create.error_log', message: e.message))
-    flash.now[:alert] = t('.critical_failure')
+    Rails.logger.error(I18n.t('controllers.orders.create.error_log',
+                              message: e.message))
+    flash.now[:alert] = t('controllers.orders.create.critical_failure')
     render 'carts/show', status: :internal_server_error
   end
 
   private
 
-  # カート情報をセット（before_action で実行）
   def set_cart
     @cart = current_cart
   end
 
-  # フォームから送信された注文情報を許可する
   def order_params
     params.require(:order).permit(
       :last_name, :first_name, :email,
