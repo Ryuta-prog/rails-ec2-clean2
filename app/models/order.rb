293841b0644ddb46aa893b2b8bfda356a13d4253
Ryuta-prog <longtaishangcun@gmail.com>
2025-06-04 00:40:25 +0900

    OrderモデルとOrdersControllerをリファクタリング：process_after_save!をpublic化しcreate_with_cart!を修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ApplicationRecord
   # ◼︎ 1. アソシエーション・バリデーション
   # ---------------------------------
   belongs_to :promotion_code, optional: true
-  has_many :order_items, dependent: :destroy
+  has_many   :order_items, dependent: :destroy
 
   validates :last_name, :first_name, :billing_address, :state, :zip, :card_name,
             :credit_card_number, :card_expiration, :card_cvv, :discounted_price, presence: true
@@ -43,17 +43,21 @@ class Order < ApplicationRecord
       order.discounted_price = cart.discounted_price(promo)
       order.promotion_code   = promo if promo&.usable?
       order.save!
-      order.process_after_save!(cart)
+      order.process_after_save!(cart) # ← ここを public にした
       order
     end
   end
 
-  # ---------------------------------
-  # ◼︎ 3. 内部用メソッド(private 以下にまとめる)
-  # ---------------------------------
-  private
+  # 注文確定後にコントローラで成功メッセージに使うクーポンコードを返す
+  def next_coupon_code
+    @next_coupon&.code
+  end
 
-  # 注文保存後に呼ばれる一連の処理
+  # ---------------------------------
+  # ◼︎ 3. 保存後の一連処理 (public)
+  # ---------------------------------
+  # ※もともと private 以下にあったが、create_with_cart! の中で直接呼びたいので
+  #   こちらを public として定義する
   def process_after_save!(cart)
     # 割引価格を再計算して確定
     self.discounted_price = cart.discounted_price(promotion_code)
@@ -84,10 +88,5 @@ class Order < ApplicationRecord
 
     # 確認メールを非同期で送信
     OrderMailer.with(order: self).confirmation_email.deliver_later
-  end
-
-  # コントローラ側で表示するクーポンコードを返す
-  def next_coupon_code
-    @next_coupon&.code
   end
 end
